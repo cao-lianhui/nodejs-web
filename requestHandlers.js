6e8ea7b5b5d97386130412c1fc93b9c517f295be
Laocao <1414841982@qq.com>
2018-07-31 16:05:38 +0800

    Laocao

---
+++ requestHandlers.js
@@ -0,0 +1,76 @@
+﻿//var exec = require("child_process").exec;
+var querystring = require("querystring"),
+      fs = require("fs"),
+      formidable = require("formidable");
+
+function start(response){
+    console.log("Request handler 'start' was called.");
+   
+    //延迟10秒返回信息，进行阻塞操作
+    /*function sleep(milliSeconds){
+        var startTime = new Date().getTime();
+        while(new Date().getTime()< startTime + milliSeconds);
+    }
+    sleep(10000);*/
+
+    /*exec("find /",{timeout:10000, maxBuffer:20000*1024},function(error, stdout, stderr){
+          response.writeHead(200,{"Content-Type":"text/plain"});
+          response.write(stdout);
+          response.end();
+    });*/
+
+    var body = '<html>' + 
+         '<head>' + 
+         '<meta http-equiv="Content-Type" content="text/html; ' +
+         'charset=UTF-8" />' +
+         '</head>'+
+         '<body>' +
+         '<form action="/upload" enctype="multipart/form-data" '+' method="post">' +
+         '<input type="file" name="upload" multiple="multiple" style="margin:100px auto 0;border:1px solid #ccc;border-radius:4px;display:block;" />'+
+         '<input type="submit" value="Upload file"  style="width:200px;height:30px;border-radius:4px;border:1px solid #ccc;margin:40px auto;background-color:#109059;display:block;"/>' +
+         '</form>' +
+         '</body>' +
+         '</html>';
+         
+         response.writeHead(200,{"Content-Type":"text/html"});
+         response.write(body);
+         response.end();
+}
+function upload(response, request){
+    console.log("Request handler 'upload' was called.");
+
+    var form = new formidable.IncomingForm();
+    form.uploadDir = 'tmp';
+    console.log("about to parse");
+    form.parse(request,function(error, fields, files){
+         if(error){throw error;}
+         console.log("parsing done");
+         fs.renameSync(files.upload.path, "/tmp/test.png");
+         response.writeHead(200,{"Content-Type":"text/html"});
+         response.write("received image:<br/>");
+         response.write("<img src='/show' />");
+         response.end();
+    });
+    /*response.writeHead(200,{"Content-Type":"text/plain"});
+    response.write("You've sent the text: "+ querystring.parse(postData).text);
+    //response.write("You've sent the text: "+ postData);
+    response.end();*/
+}
+function show(response, postData){
+    console.log("Request handler 'show' was called.");
+    fs.readFile("./tmp/test.png", "binary", function(error, file){
+        if(error){
+             response.writeHead(500,{"Content-Type":"text/plain"});
+             response.write(error + "\n");
+             response.end();
+        }else{
+             response.writeHead(200,{"Content-Type":"image/png"});
+             response.write(file,"binary");
+             response.end();
+        }
+    });
+}
+
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
---
+++ server.js
@@ -0,0 +1,32 @@
+﻿//创建一个http模块
+var http = require("http");
+//创建一个url模块
+var url = require("url");
+function start(route,handle){
+      function onRequest(request, response){
+            var postData = "";
+            var pathname = url.parse(request.url).pathname;
+            console.log(typeof handle[pathname]);
+            console.log("Request for "+pathname+" received.");
+
+            /*//设置接收的编码格式为utf-8
+            request.setEncoding("utf8");
+
+            //注册data事件监听器，收集每次接收到的新数据块
+            request.addListener("data", function(postDataChunk){
+                  postData += postDataChunk;
+                  console.log("Received POST data chunk ' " + postDataChunk + " '. ");
+            });
+
+            //接收完所有数据后触发end事件
+            request.addListener("end",function(){
+                  route(handle, pathname, response, postData);
+            });*/
+ 
+            route(handle, pathname, response, request);
+      }
+      http.createServer(onRequest).listen(8888);
+      console.log("Server has started");
+}
+//导出server模块
+exports.start = start;
